3de5436d2a6117cb5b1af6093b38cbeb5a243d09
sarahrdickerson <sarahrdickerson@gmail.com>
2023-01-27 20:29:00 -0600

    initial commit

---
+++ homework/hw1/src/ForkJoinPSort.java
@@ -0,0 +1,6 @@
+public class ForkJoinPSort {
+
+    public static void parallelSort(int[] A, int begin, int end, boolean increasing) {
+        // your implementation goes here.
+    }
+}
---
+++ homework/hw1/src/RunnablePSort.java
@@ -0,0 +1,6 @@
+public class RunnablePSort {
+
+    public static void parallelSort(int[] A, int begin, int end, boolean increasing) {
+        // your implementation goes here.
+    }
+}
