d8159aead7afe07b08b44aab575af6bd57eafb85
sarahrdickerson <sarahrdickerson@gmail.com>
2023-01-29 16:51:58 -0600

    templates for both classes

--- homework/hw1/src/ForkJoinPSort.java
+++ homework/hw1/src/ForkJoinPSort.java
@@ -1,4 +1,12 @@
-public class ForkJoinPSort {
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinPSort extends RecursiveTask<Integer>{
+
+    protected Integer compute() {
+
+        return 0;
+    }
 
     public static void parallelSort(int[] A, int begin, int end, boolean increasing) {
         // your implementation goes here.
--- homework/hw1/src/RunnablePSort.java
+++ homework/hw1/src/RunnablePSort.java
@@ -1,5 +1,8 @@
-public class RunnablePSort {
+public class RunnablePSort implements Runnable{
 
+    public void run() {
+
+    }
     public static void parallelSort(int[] A, int begin, int end, boolean increasing) {
         // your implementation goes here.
     }
