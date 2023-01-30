024eab59c425e34d339a531c268c8af3cf350644
sarahrdickerson <sarahrdickerson@gmail.com>
2023-01-29 19:10:11 -0600

    init setup for Merge class

--- homework/hw1/src/PMerge.java
+++ homework/hw1/src/PMerge.java
@@ -1,6 +1,21 @@
-public class PMerge extends Thread{
-    public void run() {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class PMerge{
+
+    private static class Merge extends Thread {
+        int[] A, B, C;
+
+        public Merge(int[] A, int[] B, int[] C) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public void run() {
+
+        }
     }
 
     public static void parallelMerge(int[] A, int[] B, int[] C, int numThreads) {
@@ -8,6 +23,15 @@ public class PMerge extends Thread{
         // These arrays may have different sizes.
         // array C is the merged array sorted in the descending order
         // your implementation goes here.
-
+        try {
+            ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+            for(int i = 0; i < numThreads; i++) {
+                threadPool.submit(new Merge(A, B, C));
+            }
+            threadPool.shutdown();
+            threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 }
