47aeef967f1245022a0528851b5d48bd37465e94
sarahrdickerson <sarahrdickerson@gmail.com>
2023-01-29 18:48:04 -0600

    init setup

--- homework/hw1/src/PMerge.java
+++ homework/hw1/src/PMerge.java
@@ -1,2 +1,13 @@
-public class PMerge {
+public class PMerge extends Thread{
+    public void run() {
+
+    }
+
+    public static void parallelMerge(int[] A, int[] B, int[] C, int numThreads) {
+        // arrays A and B are sorted in the ascending order
+        // These arrays may have different sizes.
+        // array C is the merged array sorted in the descending order
+        // your implementation goes here.
+
+    }
 }
