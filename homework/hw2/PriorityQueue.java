666a5ad24b7dc886ff477eec386bd60b2851e400
sarahrdickerson <sarahrdickerson@gmail.com>
2023-02-07 17:42:44 -0600

    starting point LOL

--- homework/hw2/PriorityQueue.java
+++ homework/hw2/PriorityQueue.java
@@ -1,10 +1,27 @@
 // tco343
 // srd2729
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.*;;
+
 public class PriorityQueue {
+        private LinkedList<Node> pQueue;
+        int maxSize;
+
+        private class Node {
+                String name;
+                int priority;
+
+                public Node(String name, int priority) {
+                        this.name = name;
+                        this.priority = priority;
+                }
+        }
 
 	public PriorityQueue(int maxSize) {
         // Creates a Priority queue with maximum allowed size as capacity
+                pQueue = new LinkedList<Node>();
+                this.maxSize = maxSize;
 	}
 
 	public int add(String name, int priority) {
@@ -12,18 +29,19 @@ public class PriorityQueue {
         // Returns the current position in the list where the name was inserted;
         // otherwise, returns -1 if the name is already present in the list.
         // This method blocks when the list is full.
-        return 0;
+                return 0;
+                
 	}
 
 	public int search(String name) {
         // Returns the position of the name in the list;
         // otherwise, returns -1 if the name is not found.
-        return 0;
+                return 0;
 	}
 
 	public String getFirst() {
         // Retrieves and removes the name with the highest priority in the list,
         // or blocks the thread if the list is empty.
-        return "";
+                return "";
 	}
 }
