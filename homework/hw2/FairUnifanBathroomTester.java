3c3d68061f10c7a365a993767ba6ef72a3bc8988
sarahrdickerson <sarahrdickerson@gmail.com>
2023-02-08 14:50:07 -0600

    renamed class name typo

--- homework/hw2/FairUnifanBathroomTester.java
+++ homework/hw2/FairUnifanBathroomTester.java
@@ -1,4 +1,4 @@
-public class FairUnfanBathroomTester {
+public class FairUnifanBathroomTester {
     public static void main(String[] args) throws InterruptedException {
         FairUnifanBathroom bathroom = new FairUnifanBathroom();
         int numThreads = 20;
